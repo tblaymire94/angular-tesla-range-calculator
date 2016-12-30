4481e78c17202b67bbb2157f91d69a62bb5694c4
Thomas Blaymire <thomas.blaymire@hermes-europe.co.uk>
2016-12-30 13:47:10 +0000

    Implemented styles for battery component and applied config

--- src/app/tesla-battery/tesla-battery.module.ts
+++ src/app/tesla-battery/tesla-battery.module.ts
@@ -4,21 +4,30 @@
 
 import { CommonModule } from '@angular/common';
 import { ngModule } from '@angular/core';
-import { ReactiveFormsModule } from 'angular/forms';
+import { ReactiveFormsModule } from '@angular/forms';
+
+// containers
+import { TeslaBatteryComponent } from './containers/tesla-battery/tesla-battery.component';
 
 // services
 import { BatteryService } from './tesla-battery.service';
 
-@ngModule({
-    declarations: [],
-    imports: [
-        CommonModule,
-        ReactiveFormsModule
-    ],
-    providers: [
-        // add the service to our sub-module
-        BatteryService
-    ],
-    exports: []
+@NgModule({
+  declarations: [
+    // registering our container component
+    TeslaBatteryComponent
+  ],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule
+  ],
+  providers: [
+    // add the service to our sub-module
+    BatteryService
+  ],
+  exports: [
+    // exporting so our root module can access
+    TeslaBatteryComponent
+  ]
 })
 export class TeslaBatteryModule {}
